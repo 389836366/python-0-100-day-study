151037f27dea6bc1265971f23f97633bc45e9ff3
yongyangyy <2955520958@qq.com>
2025-05-03 20:54:34 +0800

    创建列表

---
+++ 2025year/1day-20days/Day8/Day8-1.py
@@ -0,0 +1,14 @@
+#创建列表
+items1 = [35, 12, 99, 68, 55, 35, 87]
+items2 = ['Python', 'Java', 'Go', 'Kotlin']
+items3 = [100, 12.3, 'Python', True]
+print(items1)
+print(items2)
+print(items3)
+items4 = list(range(1, 10))
+items5 = list('hello')
+print(items4)
+print(items5)
+
+
+
