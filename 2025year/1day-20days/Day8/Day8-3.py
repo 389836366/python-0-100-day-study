9f77fc0e7af71470305ef54cf95bc546f4ce157e
yongyangyy <2955520958@qq.com>
2025-05-03 21:16:08 +0800

    列表的遍历

---
+++ 2025year/1day-20days/Day8/Day8-3.py
@@ -0,0 +1,9 @@
+#元素的遍历
+languages = ['Python', 'Java', 'C++', 'Kotlin']
+for index in range(len(languages)):
+    print(languages[index])
+
+languages = ['Python', 'Java', 'C++', 'Kotlin']
+for language in languages:
+    print(language)
+
