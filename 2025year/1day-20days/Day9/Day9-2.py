11ca38f2d7147c990e4e3942354906c53c06882d
yongyangyy <2955520958@qq.com>
2025-05-04 20:35:20 +0800

    元素位置和频次

---
+++ 2025year/1day-20days/Day9/Day9-2.py
@@ -0,0 +1,8 @@
+#元素位置和频次
+items = ['Python', 'Java', 'Java', 'C++', 'Kotlin', 'Python']
+print(items.index('Python'))
+print(items.index('Python', 1))
+print(items.count('Python'))
+print(items.count('Kotlin'))
+print(items.count('Swfit'))
+print(items.index('Java', 3))
