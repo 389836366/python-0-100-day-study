68d15de3e5e02233095b59cbb12ebd52b46d2f8e
yongyangyy <2955520958@qq.com>
2025-05-07 09:46:19 +0800

    字典的应用

---
+++ 2025year/1day-20days/Day13/Day13-4.py
@@ -0,0 +1,22 @@
+# 字典的应用
+# 输入一段话，统计每个英文字母出现的次数，按出现次数从高到低输出。
+sentence = input('请输入一段话:')
+counter = {}
+for ch in sentence:
+    if 'A' <= ch <= 'Z' or 'a' <= ch <= 'z':
+        counter[ch] = counter.get(ch, 0)+1
+sorted_keys = sorted(counter, key=counter.get, reverse=True)
+for key in sorted_keys:
+    print(f'{key}出现了{counter[key]}次')
+
+stocks = {
+    'AAPL': 191.88,
+    'GOOG': 1186.96,
+    'IBM': 149.24,
+    'ORCL': 48.44,
+    'ACN': 166.89,
+    'FB': 208.09,
+    'SYMC': 21.29
+}
+stocks2 = {key: value for key, value in stocks.items() if value > 100}
+print(stocks2)
