3defd699cc5dec2c7dfe8298990a2865ff312594
yongyangyy <2955520958@qq.com>
2025-05-10 15:10:08 +0800

    高阶函数

---
+++ 2025year/1day-20days/Day16/Day16-1.py
@@ -0,0 +1,14 @@
+# 高阶函数
+def is_even(num):
+    """判断num是不是偶数"""
+    return num % 2 == 0
+
+
+def square(num):
+    """求平方"""
+    return num ** 2
+
+
+old_nums = [35, 12, 8, 99, 60, 52]
+new_nums = list(map(square, filter(is_even, old_nums)))
+print(new_nums)  # [144, 64, 3600, 2704]
